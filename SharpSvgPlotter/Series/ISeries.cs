7d5d82870975148902b9b35d99b6a89d6e4cdafe
Cemonix <gittlermilan@seznam.cz>
2025-04-28 18:07:32 +0200

    Add ISeries interface to define series properties and data range calculation

---
+++ SharpSvgPlotter/Series/ISeries.cs
@@ -0,0 +1,12 @@
+using System;
+using SharpSvgPlotter.Primitives;
+
+namespace SharpSvgPlotter.Series;
+
+public interface ISeries
+{
+    string Title { get; init; }
+    PlotStyle PlotStyle { get; init; }
+    IEnumerable<DataPoint> DataPoints { get; init; }
+    (double minX, double maxX, double minY, double maxY) GetDataRange();
+}
