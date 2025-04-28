e0f20a5ad1fa058c9034474f2c45879feee4c527
Cemonix <gittlermilan@seznam.cz>
2025-04-28 18:05:45 +0200

    Add PlotArea class to define a rectangular plotting area with dimensions and area calculation

---
+++ SharpSvgPlotter/Primitives/PlotArea.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace SharpSvgPlotter.Primitives;
+
+public class PlotArea(double width, double height, PlotMargins margins)
+{
+    private readonly DataPoint _origin = new (margins.Left, margins.Top);
+    private readonly double _width = width - margins.Left - margins.Right;
+    private readonly double _height = height - margins.Top - margins.Bottom;
+
+    public double X => _origin.X;
+    public double Y => _origin.Y;
+    public double Width => _width; 
+    public double Height => _height;
+    public double Area => _width * _height;
+}
