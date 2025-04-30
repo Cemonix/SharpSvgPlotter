8172e8731ef28d0ddc6fe1f30ce1b7bf6be54c92
Cemonix <gittlermilan@seznam.cz>
2025-04-30 13:16:42 +0200

    Add FromList method to DataPoint for creating instances from coordinate lists

--- SharpSvgPlotter/Primitives/DataPoint.cs
+++ SharpSvgPlotter/Primitives/DataPoint.cs
@@ -11,4 +11,15 @@ public readonly struct DataPoint(double x, double y)
     {
         return $"({X}, {Y})";
     }
+
+    public static IEnumerable<DataPoint> FromList(List<double> xData, List<double> yData)
+    {
+        if (xData.Count != yData.Count)
+            throw new ArgumentException("X and Y data lists must have the same length.");
+
+        for (int i = 0; i < xData.Count; i++)
+        {
+            yield return new DataPoint(xData[i], yData[i]);
+        }
+    }
 }
