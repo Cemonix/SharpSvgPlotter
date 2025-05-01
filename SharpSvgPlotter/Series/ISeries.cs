3d11a2dd2f22e034f159f7de075a1f69c1ef87ec
Cemonix <gittlermilan@seznam.cz>
2025-05-01 14:47:50 +0200

    Refactor ISeries interface to include detailed XML documentation for properties and methods

--- SharpSvgPlotter/Series/ISeries.cs
+++ SharpSvgPlotter/Series/ISeries.cs
@@ -1,13 +1,36 @@
 using System;
+using SharpSvgPlotter.Components;
 using SharpSvgPlotter.Primitives;
-using SharpSvgPlotter.Primitives.PlotStyles;
+using SharpSvgPlotter.Styles;
 
 namespace SharpSvgPlotter.Series;
 
 public interface ISeries
 {
+    /// <summary>
+    /// Title used for the series and the legend.
+    /// </summary>
     string Title { get; init; }
-    PlotStyle PlotStyle { get; init; }
-    IEnumerable<DataPoint> DataPoints { get; init; }
-    (double minX, double maxX, double minY, double maxY) GetDataRange();
+
+    /// <summary>
+    /// Base styling information
+    /// </summary>
+    PlotStyle PlotStyle { get; init; } // Use the base PlotStyle type
+
+    /// <summary>
+    /// Performs any necessary pre-calculations before axis scaling.
+    /// (e.g., Histogram binning).
+    /// </summary>
+    void PrepareData();
+
+    /// <summary>
+    /// Gets the minimum and maximum data values this series contributes to an axis.
+    /// This is called *after* PrepareData.
+    /// </summary>
+    /// <param name="axisType">Which axis (X or Y) bounds are required.</param>
+    /// <returns>
+    /// A tuple (min, max) for the specified axis. 
+    /// Returns (NaN, NaN) if the series doesn't contribute to the axis or has no data.
+    /// </returns>
+    (double Min, double Max) GetAxisBounds(AxisType axisType);
 }
